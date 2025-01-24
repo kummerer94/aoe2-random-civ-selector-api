0ed7a9d8419c47b964e8e973a02f03d3d2305cef
Alexander Kammerer <alexander.kammerer@finccam.com>
2025-01-24 14:23:00 +0100

    Log more attributes

--- aoe2-random-civ-api/api/load.js
+++ aoe2-random-civ-api/api/load.js
@@ -8,6 +8,8 @@ module.exports = async (req, res) => {
   const { user = "standard" } = req.query;
   const db = await connectToDatabase(process.env.MONGODB_CONN_STR);
   const collection = await db.collection("configurations");
+  console.log("Collection: ", collection);
+  console.log("User: ", user);
   const cursor = collection.find({ user }).sort({ inserted: -1 }).limit(1);
   let configurations = await cursor.toArray();
   console.log("# configurations: ", configurations.length);
